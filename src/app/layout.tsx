1b312a27960217a523c59f41ac6714a79e09bc4c
Mateus Azevedo <99230374+mateuscorreiaazevedo@users.noreply.github.com>
2023-06-17 01:40:15 -0300

    chore: adjusting app folder organization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,24 @@
 import { Roboto_Flex as Roboto } from 'next/font/google'
 import '../assets/styles/globals.css'
+import { ReactNode } from 'react'
 
-const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+const roboto = Roboto({
+  subsets: ['latin'],
+  variable: '--font-roboto',
+  fallback: ['sans serif']
+})
 
 export const metadata = {
   title: '@mateuscorreiaazevedo',
   description: 'My boilerplate using Next.js 13. Powered by Mateus Azevedo'
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="pt">
-      <body className={`${roboto.variable} min-h-screen w-full`}>{children}</body>
+    <html lang="pt-br">
+      <body className={`${roboto.variable} min-h-screen w-full font-roboto`}>
+        {children}
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Page() {
-  return (
-    <p className="mx-4 flex flex-col items-center justify-center bg-sky-500 text-2xl">
-      opa
-    </p>
-  )
-}

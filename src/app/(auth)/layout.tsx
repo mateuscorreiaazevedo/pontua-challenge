721e4a7c8e3b9166a027c3cfae09a759e510ff1e
Mateus Azevedo <99230374+mateuscorreiaazevedo@users.noreply.github.com>
2023-07-26 16:38:59 -0300

    chore(auth): implement middleware on authenticated routes

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,15 +1,18 @@
 import images from '@/assets/images'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen w-full items-center justify-center bg-blue-800 font-epilogue">
-      <Image
-        src={images.pontuaWhite}
-        alt="Logo Pontua White"
-        className="fixed left-28 top-12"
-      />
+      <Link href="/">
+        <Image
+          src={images.pontuaWhite}
+          alt="Logo Pontua White"
+          className="fixed left-28 top-12"
+        />
+      </Link>
       {children}
     </div>
   )
---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export default function middleware(req: NextRequest) {
+  const token = req.cookies.get('pontua.token')?.value
+
+  if (!token) {
+    return NextResponse.redirect(new URL('/login', req.nextUrl))
+  }
+}
+
+export const config = {
+  matcher: ['/']
+}
